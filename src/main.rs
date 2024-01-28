e21489cc4cb3e65233b5015cfbd6a56b52728cda
phicode <davprocode@gmail.com>
2024-01-28 16:18:00 +0100

    Feat : dowload button for JSON File in file system

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
-use druid::{AppLauncher, PlatformError, Widget, WidgetExt, WindowDesc};
+use druid::{AppLauncher, PlatformError, Widget, WidgetExt, WindowDesc };
 use druid::widget::{Flex, TextBox, Button};
 use druid::Lens;
+use druid::commands::SHOW_OPEN_PANEL;
+use druid::FileDialogOptions;
+use druid_shell::FileSpec;
 use std::fs::File;
 use std::io::Write;
 use serde_json::Value;
 
 #[derive(Clone)]
 pub struct AppState {
-    table_name: String
+    table_name: String,
+    file_content: String,
 }
 
 struct TableNameLens;
@@ -22,18 +26,66 @@ impl Lens<AppState, String> for TableNameLens {
     }
 }
 
-
 impl Default for AppState {
     fn default() -> Self {
         AppState {
             table_name: String::new(),
+            file_content: String::new(),
         }
     }
 }
 
 impl druid::Data for AppState {
     fn same(&self, other: &Self) -> bool {
-        self.table_name.same(&other.table_name)
+        self.table_name.same(&other.table_name) && self.file_content.same(&other.file_content)
+    }
+}
+
+struct MyAppDelegate;
+
+impl druid::AppDelegate<AppState> for MyAppDelegate {
+    fn command(
+        &mut self,
+        ctx: &mut druid::DelegateCtx,
+        _target: druid::Target,
+        cmd: &druid::Command,
+        data: &mut AppState,
+        _env: &druid::Env,
+    ) -> druid::Handled {
+        match cmd {
+            x if x.is(crate::COMMAND_CONVERT) => {
+                println!("Starting conversion...");
+                if !data.file_content.is_empty() {
+                    match serde_json::from_str::<Vec<Value>>(&data.file_content) {
+                        Ok(parsed_json) => {
+                            let table_name = &data.table_name;
+                            let columns: Vec<&str> = parsed_json.first().unwrap().as_object().unwrap().keys().map(|s| s.as_ref()).collect();
+            
+                            let mut output_file = File::create(format!("{}.sql", table_name)).expect("Unable to create SQL File");
+            
+                            for i in 0..parsed_json.len() {
+                                let item = &parsed_json[i];
+                                let values: Vec<String> = columns.iter().map(|col| item[col].to_string()).collect();
+                                let sql_query = format!("INSERT INTO {} ({}) VALUES ({});\n", table_name, columns.join(", "), values.join(", "));
+                                output_file.write_all(sql_query.as_bytes()).expect("Unable to write to SQL file");
+                            }
+            
+                            ctx.submit_command(crate::COMMAND_SHOW_MESSAGE.with(data.clone()));
+                            println!("Conversion completed successfully");
+                        }
+                        Err(err) => {
+                            ctx.submit_command(crate::COMMAND_SHOW_ERROR.with(data.clone()));
+                            println!("Error parsing JSON: {:?}", err);
+                        }
+                    }
+                } else {
+                    println!("File content is empty. Cannot parse an empty JSON file.");
+                }
+                druid::Handled::Yes
+            }
+
+            _ => druid::Handled::No,
+        }
     }
 }
 
@@ -43,28 +95,9 @@ fn main() -> Result<(), PlatformError> {
         .window_size((800.0, 400.0));
 
     AppLauncher::with_window(main_window)
+        .delegate(MyAppDelegate)
         .log_to_console()
         .launch(AppState::default())
-}
-
-fn convert_and_save(ctx: &mut druid::EventCtx, data: &mut AppState, file_content: String) {
-    if let Ok(parsed_json) = serde_json::from_str::<Vec<Value>>(&file_content) {
-        let table_name = &data.table_name;
-        let columns: Vec<&str> = parsed_json.first().unwrap().as_object().unwrap().keys().map(|s| s.as_ref()).collect();
-
-        let mut output_file = File::create(format!("{}.sql", table_name)).expect("Unable to create SQL File");
-
-        for i in 0..parsed_json.len() {
-            let item = &parsed_json[i];
-            let values: Vec<String> = columns.iter().map(|col| item[col].to_string()).collect();
-            let sql_query = format!("INSERT INTO {} ({}) VALUES ({});\n", table_name, columns.join(", "), values.join(", "));
-            output_file.write_all(sql_query.as_bytes()).expect("Unable to write to SQL file");
-        }
-
-        ctx.submit_command(crate::COMMAND_SHOW_MESSAGE.with(data.clone()));
-    } else {
-        ctx.submit_command(crate::COMMAND_SHOW_ERROR.with(data.clone()));
-    }
 }
 
 fn build_ui() -> Box<dyn Widget<AppState>> {
@@ -72,20 +105,30 @@ fn build_ui() -> Box<dyn Widget<AppState>> {
         .with_placeholder("Enter table name")
         .lens(TableNameLens);
 
+    let open_button = Button::new("Download JSON File")
+        .on_click(|ctx, _data: &mut AppState, _env| {
+            println!("Starting uploaded...");
+            let json = FileSpec::new("JSON Files", &["json"]);
+            let file_dialog_options = FileDialogOptions::new()
+                .allowed_types(vec![json])
+                .default_type(json);
+
+            let file_dialog_options_clone = file_dialog_options.clone();
+            ctx.submit_command(SHOW_OPEN_PANEL.with(file_dialog_options_clone));
+        });
+
     let convert_button = Button::new("Convert")
-        .on_click(|ctx, data: &mut crate::AppState, _| {
-            let file_content = r#"[
-                {"name": "John", "age": 30, "city": "New York"},
-                {"name": "Alice", "age": 25, "city": "San Francisco"}
-            ]"#
-            .to_string();
-            convert_and_save(ctx, data, file_content);
+        .on_click(|ctx, data: &mut AppState, _env| {
+            ctx.submit_command(crate::COMMAND_CONVERT.with(data.clone()));
         });
 
     let content: Box<dyn Widget<_>> = Box::new(
         Flex::column()
+            .with_spacer(100.0)
             .with_child(table_name)
-            .with_spacer(10.0)
+            .with_spacer(100.0)
+            .with_child(open_button)
+            .with_spacer(50.0)
             .with_child(convert_button)
             .boxed()   
     );
@@ -96,3 +139,4 @@ fn build_ui() -> Box<dyn Widget<AppState>> {
 pub const COMMAND_CONVERT: druid::Selector<AppState> = druid::Selector::new("app.convert");
 pub const COMMAND_SHOW_MESSAGE: druid::Selector<AppState> = druid::Selector::new("app.show-message");
 pub const COMMAND_SHOW_ERROR: druid::Selector<AppState> = druid::Selector::new("app.show-error");
+pub const COMMAND_SET_FILE_PATH: druid::Selector<String> = druid::Selector::new("app.set-file-path");
